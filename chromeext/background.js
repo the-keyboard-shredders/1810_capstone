934b89805eb754f9b36f8afa271b64297ef06555
Rui Z <rrzhao13@gmail.com>
2019-01-29 14:54:32 -0500

    background is able to grab email from chrome storage

--- chromeext/background.js
+++ chromeext/background.js
@@ -41,3 +41,7 @@
 chrome.browserAction.onClicked.addListener(function() {
   chrome.tabs.create({ url: "index.html" });
 });
+
+chrome.storage.sync.get("email", function(result) {
+  console.log("RESULT: ", result.email);
+});
--- chromeext/oauth.js
+++ chromeext/oauth.js
@@ -1,11 +1,6 @@
 // document.addEventListener("DOMContentLoaded", function() {
 //   document.querySelector("button").addEventListener("click", function() {
-//     //   chrome.tabs.query({ currentWindow: true, active: true }, function(tabs) {
-//     //     chrome.tabs.sendMessage(tabs[0].id, "hi");
-//     //   });
-//     // });
 //     chrome.identity.getAuthToken({ interactive: true }, function(token) {
-//       console.log("Token", token);
 //       let init = {
 //         method: "GET",
 //         async: true,
@@ -15,34 +10,41 @@
 //         },
 //         contentType: "json"
 //       };
+//       fetch("https://www.googleapis.com/oauth2/v1/userinfo?alt=json", init)
+//         .then(response => response.json())
+//         .then(function(data) {
+//           const email = data.email;
+//           chrome.storage.sync.set({ email: email }, function() {
+//             if (chrome.runtime.error) {
+//               console.log("Runtime error.");
+//             }
+//           });
+//           window.close();
+//         });
 //     });
 //   });
 // });
 
 window.onload = function() {
   document.querySelector("button").addEventListener("click", function() {
-    chrome.tabs.query({ currentWindow: true, active: true }, function(tabs) {
-      let email = "";
-      chrome.identity.getAuthToken({ interactive: true }, function(token) {
-        let init = {
-          method: "GET",
-          async: true,
-          headers: {
-            Authorization: "Bearer " + token,
-            "Content-Type": "application/json"
-          },
-          contentType: "json"
-        };
-        fetch("https://www.googleapis.com/oauth2/v1/userinfo?alt=json", init)
-          .then(response => response.json())
-          .then(function(data) {
-            email = data.email;
-            console.log("DATA", data);
-            console.log("EMAIL", email);
-          });
-      });
-      console.log("TABS: ", tabs);
-      chrome.tabs.sendMessage(tabs[0].id, { email: email });
+    chrome.identity.getAuthToken({ interactive: true }, function(token) {
+      let init = {
+        method: "GET",
+        async: true,
+        headers: {
+          Authorization: "Bearer " + token,
+          "Content-Type": "application/json"
+        },
+        contentType: "json"
+      };
+      fetch("https://www.googleapis.com/oauth2/v1/userinfo?alt=json", init)
+        .then(response => response.json())
+        .then(function(data) {
+          const email = data.email;
+          chrome.storage.sync.set({ email: email });
+          console.log("DATA", data);
+          console.log("EMAIL", email);
+        });
     });
   });
 };
